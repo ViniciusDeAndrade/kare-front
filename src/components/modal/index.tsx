78fe1e5a0f5779b954bf6112a796309b623d0efa
Marcos Vinicius de Araujo Andrade <marcos.andrade@programmers.com.br>
2021-11-17 12:47:46 -0300

    adjusts

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,4 +1,6 @@
-import { Typography, Modal, Box } from '@material-ui/core';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 import { BoxStyled } from './styles';
 import Botao from '../button';
@@ -8,46 +10,45 @@ interface Render {
     hide: Function
 }
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
-
 export default function Popup(props: Render): JSX.Element {
     return (
-            <Modal 
-                open={props.show}
-                onClose={() => props.hide}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
-                    <Botao 
-                        o={
-                            ({
-                                onClick: () => props.hide(),
-                                text:"close", 
-                                color:"warning", 
-                                variant:"outlined"
-                            })
-                        }
-                    />
-                </Box>
-                
-            </Modal>
+        <Modal
+            open={props.show}
+            onClose={() => props.hide}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={
+                {
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 400,
+                    bgcolor: 'background.paper',
+                    border: '2px solid #000',
+                    boxShadow: 24,
+                    p: 4,
+                }
+            }>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Text in a modal
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                </Typography>
+                <Botao
+                    o={
+                        ({
+                            onClick: () => props.hide(),
+                            text: "close",
+                            color: "warning",
+                            variant: "outlined"
+                        })
+                    }
+                />
+            </Box>
+
+        </Modal>
     );
 }
