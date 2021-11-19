040947761328cebd0fcc1eaededc2c320fd95cad
Vinicius de Andrade <viniciusclo@gmail.com>
2021-11-19 13:58:47 -0300

    starting signup page

---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface SignupProps {
+    
+}
+ 
+const Signup: FunctionComponent<SignupProps> = () => {
+    return ( <div>teste</div> );
+}
+ 
+export default Signup;
