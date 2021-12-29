22dedae9badb5e8b8cc49a77ae3ba0a26ecf4b9c
Marcos Vinicius de Araujo Andrade <m.de.araujo.andrade@avanade.com>
2021-12-29 10:42:22 -0300

    removing custom button

--- src/components/button/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-
-export const StyledButton = styled.div`
-   margin: 0 0 0 10px;
-`;
--- src/components/interfaces/ButtonProps.ts
+++
@@ -1,9 +0,0 @@
-export interface ButtonProps {
-    o?: {
-        onClick: Function,
-        text: String,
-        type?: String,
-        variant?: 'text' | 'outlined' | 'contained',
-        color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning'
-    }
-}
