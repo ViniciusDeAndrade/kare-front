cc548c06f2c5b0d68870f660fada4796af88374b
Vinicius de Andrade <viniciusclo@gmail.com>
2022-01-15 11:44:01 -0300

    dashboard spacing

--- src/components/DashboardBodySelector/index.tsx
+++ src/components/DashboardBodySelector/index.tsx
@@ -1,3 +1,5 @@
+import { Container } from '@mui/material';
+import { Box } from '@mui/system';
 import { profile } from 'console';
 import { FunctionComponent } from 'react';
 import Balance from '../Balance';
@@ -9,14 +11,18 @@ interface DashboardBodySelectorProps {
 const DashboardBodySelector: FunctionComponent<DashboardBodySelectorProps> = (props) => {
     const render = props.render;
     return (
-        <div>{render == "profile" ? (
-            <div>foi profile</div>
-        ) 
-        : (
-            <Balance/>
-          )
-        }
-        </div>
+        <Container>
+            <Box sx={{ mt: 1 }}>
+
+                {render == "profile" ? (
+                    <div>foi profile</div>
+                    ) 
+                    : (
+                        <Balance/>
+                        )
+                    }
+            </Box>
+        </Container>
     );
 }
  
