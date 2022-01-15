d1b3f5f7f4f20c2d0b660ccb588fb96f5d782911
Vinicius de Andrade <viniciusclo@gmail.com>
2022-01-15 11:27:48 -0300

    style header

---
+++ src/components/header/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+import { AppBar, Toolbar } from "@mui/material";
+
+export const HeaderStyled = styled(AppBar)`
+    background-color: lightgray;
+    color: red;
+    padding: 10px;
+`
+
+export const ToolBarStyled = styled(Toolbar)`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
